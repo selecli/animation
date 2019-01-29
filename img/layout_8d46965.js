b621ac59aa33c67224f6e9a01aa774fe39dc10bb
selecli <dachali1@sina.com>
2019-01-29 16:05:08 +0800

    首页动画

---
+++ img/layout_8d46965.js
@@ -0,0 +1,1 @@
+!function(){function e(){n.style.fontSize=n.clientWidth>1e3?(Math.min(1440,n.clientWidth)*t).toFixed(2)+"px":"24px"}var t=24/1440,n=document.documentElement;e(),window.addEventListener("resize",e)}();
